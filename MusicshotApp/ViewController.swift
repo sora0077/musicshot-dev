952af2e3004afcc1592347dbcc804716c45e553d
sora0077 <t.hayashi0077+github@gmail.com>
2018-06-14 22:06:06 +0900

    Delete curret files

---
+++ MusicshotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicshotApp
+//
+//  Created by 林達也 on 2018/06/14.
+//  Copyright © 2018年 林達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
